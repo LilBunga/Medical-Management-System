abcc1e0cd0142bb9f5de8098925a53286b242a0a
LilBunga <106434602+LilBunga@users.noreply.github.com>
2024-10-08 00:40:24 +0700

    upload main

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main
+{
+    public static void main(String args[]) 
+    {
+       new Welcome();
+    }
+}
